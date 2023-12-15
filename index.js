d4e582a80fe91c53bcfb8ae4508cc192d7d493f1
regianebarbosa <128555814+regianebarbosa@users.noreply.github.com>
2023-12-14 23:22:14 -0300

    Update index.js
    Desafio da DIO do Felipão pronto.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log ("olá")
+let nomeHeroi = "Homem de ferro ";
+let xp = 11000;
+
+if (xp < 1000) {
+    xp = "Ferro";
+}else if (xp >= 1001 && xp <= 2000) {
+    xp = "Bronze"; 
+}else if (xp >= 2001 && xp <= 5000) {
+    xp = "Prata"; 
+}else if (xp >= 5001 && xp <= 7000) {
+    xp = "Ouro";
+}else if (xp >= 7001 && xp <= 8000) {
+    xp = "Platina";
+}else if (xp >= 8001 && xp <= 9000) {
+    xp = "Ascendente";
+}else if (xp >= 9001 && xp <= 10000) {
+    xp = "Imortal";
+}else
+    xp = "Radiante";
+
+    console.log ("O herói de nome " + nomeHeroi + "está no nível de " + xp);
